9859cda0c1c243e4ef6e4dff6d927a8dd7670d75
Juan Haro <jcharomo@gmail.com>
2023-07-05 06:20:46 -0700

    Level 1 update

--- Assets/Scripts/Character/GGC_Scripts/PlayerMovement.cs
+++ Assets/Scripts/Character/GGC_Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float jumpingPower = 22f;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
