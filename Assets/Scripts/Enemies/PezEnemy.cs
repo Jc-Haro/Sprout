416bb72f9c8eef2c28ac3bc28f1bb86f5c569250
Juan Haro <jcharomo@gmail.com>
2023-07-19 07:54:00 -0700

    Beta 2.0

--- Assets/Scripts/Enemies/PezEnemy.cs
+++ Assets/Scripts/Enemies/PezEnemy.cs
@@ -5,12 +5,62 @@ using UnityEngine;
 public class PezEnemy : MonoBehaviour
 {
 
-    public float speed = 5f; // The speed at which the object moves
 
-    private void Update()
+
+    public float horizontalSpeed = 3f;   // Speed of horizontal movement
+    public float verticalSpeed = 3f;     // Speed of vertical movement
+    public float horizontalDuration = 3f; // Duration of horizontal movement
+    public float verticalDuration = 3f;   // Duration of vertical movement
+
+    private Vector3 initialPosition;      // Initial position of the object
+
+    private void Start()
     {
-        // Move the object from left to right
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        initialPosition = transform.position;
+        StartCoroutine(MovementCoroutine());
     }
 
+    private System.Collections.IEnumerator MovementCoroutine()
+    {
+        while (true)
+        {
+            // Move horizontally from left to right
+            float horizontalTimeElapsed = 0f;
+            while (horizontalTimeElapsed < horizontalDuration)
+            {
+                transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+                horizontalTimeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // Move vertically
+            float verticalTimeElapsed = 0f;
+            while (verticalTimeElapsed < verticalDuration)
+            {
+                transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime);
+                verticalTimeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // Move horizontally for 3 seconds
+            horizontalTimeElapsed = 0f;
+            while (horizontalTimeElapsed < horizontalDuration)
+            {
+                transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+                horizontalTimeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // Return to the ground
+            transform.position = new Vector3(transform.position.x, initialPosition.y, transform.position.z);
+        }
+    }
+
+
+
+
+
+
+
 }
+
